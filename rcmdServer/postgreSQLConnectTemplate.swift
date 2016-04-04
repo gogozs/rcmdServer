43e0b93fd083ddf657c370e4fe7834f61761e9e4
Song Zhou <zhousong1993@gmail.com>
2016-04-04 23:39:46 +0800

    added postgreSQL connect template file

---
+++ rcmdServer/postgreSQLConnectTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  postgreSQLConnectTemplate.swift
+//  rcmdServer
+//
+//  Created by Song Zhou on 4/4/16.
+//  Copyright © 2016 Song Zhou. All rights reserved.
+//
+
+import Foundation
+
+/// Copy this file and rename to postgreSQLConnect.swift
+
+//let dbhost: String = "localhost"
+//let dbusername: String = "your database user name"
+//let dbuserpassword: String = "yuour database passwd"
+//let dbname: String = "your database name"
+//
+//let connectInfo = ("host='\(dbhost)' dbname='\(dbname)' user='\(dbusername)' password='\(dbuserpassword)'")
